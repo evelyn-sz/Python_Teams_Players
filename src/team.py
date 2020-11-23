76653a81c5dd4f4e94c468473e63bc3fcd2928ad
evelyn-sz <evelynszarek@gmail.com>
2020-11-23 16:01:06 +0000

    Test 7 find_player_in_team

--- src/team.py
+++ src/team.py
@@ -6,3 +6,14 @@ class Team:
     
     def add_player(self, name):
         self.players.append(name)
+    
+    def has_player(self, name):
+        for player in self.players:
+            if player == name:
+                return True
+        else: 
+            return False
+
+    # same as above but in short line:
+    # return name in self.players
+
